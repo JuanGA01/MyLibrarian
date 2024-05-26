ce2f8d5e3f2572c2117fa7a4ddfc67722e3ffc4a
Juan García Álvarez <jga2001@live.com>
2024-05-26 18:17:24 +0200

    Otra nueva versión

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,8 +69,4 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
 
-    implementation("com.google.accompanist:accompanist-permissions:0.23.1")
-    implementation("com.google.accompanist:accompanist-insets:0.23.1")
-    implementation("androidx.activity:activity-compose:1.4.0")
-
 }
